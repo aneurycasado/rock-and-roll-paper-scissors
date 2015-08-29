07c7fe281ae0cbf10b51459883f07b1f514ed369
Aneury Casado <aneury.casado@gmail.com>
2015-08-29 18:05:33 -0400

    Got music working

--- frontend/js/audio.js
+++ frontend/js/audio.js
@@ -3,10 +3,24 @@
 // })
 
 addEventListener('keydown', function(key){
+  var rand = Math.floor(Math.random()*5+1)
+  var audio = document.getElementById('audio');
+  var source = document.getElementById('currentSong');
   if(key.keyCode === 49){
-    var rand = Math.floor(Math.random()*5);
-    console.log(rand);
-    console.log($('#currentSong'));
-    $("#currentSong").attr("src","/audio/rock/"+rand+".mp3");
+    $("#genre").html("rock");
+    source.src= "/audio/rock/"+rand+".mp3"
+    audio.load(); //call this to just preload the audio without playingaudio.play(); //ca
+    audio.play();
+  }else if(key.keyCode === 50){
+    $("#genre").html("country");
+    source.src= "/audio/country/"+rand+".mp3"
+    audio.load(); //call this to just preload the audio without playingaudio.play(); //ca
+    audio.play();
+  }else if(key.keyCode === 51){
+    $("#genre").html("top40");
+    source.src= "/audio/top40/"+rand+".mp3"
+    audio.load(); //call this to just preload the audio without playingaudio.play(); //ca
+    audio.play();
   }
+
 });
